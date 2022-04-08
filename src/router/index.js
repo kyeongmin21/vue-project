813babe64b78f20fd7c932dd27b5154cb359ae5f
kim kyeong min <kyeongmin21@naver.com>
2022-04-08 22:58:19 +0900

    DataBinding : 컴포넌트추가, 라우터링크 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" webpackPrefetch: true */ '@/views/About.vue')
+  },
+  {
+    path: '/dataBinding',
+    name: 'DataBinding',
+    component: () => import(/* webpackChunkName: "dataBinding" webpackPrefetch: true */ '@/views/DataBinding.vue')
   }
 ]
 
---
+++ src/views/DataBinding.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dataBinding">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBinding',
+  components: {}
+}
+</script>
