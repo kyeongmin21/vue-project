8ee4af240d77e0b3fe776972da43efd16b3d1410
kim kyeong min <kyeongmin21@naver.com>
2022-04-11 22:13:39 +0900

    mixins 폴더 추가

---
+++ src/mixins/mixins.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export default {
+  methods: {
+    async api (url, method, data) {
+      return (await axios({
+        method: 'get',
+        url: 'https://214db7a1-74cc-4b71-879f-04b14f15e282.mock.pstmn.io/test',
+        data
+      }).then((response) => {
+        console.log(response.data)
+      }).catch(e => {
+        console.log(e)
+      })).data
+    }
+  }
+}
