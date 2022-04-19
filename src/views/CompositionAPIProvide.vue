42d5dfeb80bce7f81a971be821102acc44a6f821
kim kyeong min <kyeongmin21@naver.com>
2022-04-19 22:47:50 +0900

    provide
    - 부모 컴포넌트에서는 provide함수를 통해서 전달할 값에 대한 key, value 설정

---
+++ src/views/CompositionAPIProvide.vue
@@ -0,0 +1,17 @@
+<template>
+  <CompositionAPIInject/>
+</template>
+
+<script>
+import { provide } from 'vue'
+import CompositionAPIInject from '@/views/CompositionAPIInject'
+
+export default {
+  components: {
+    CompositionAPIInject
+  },
+  setup () {
+    provide('title', 'vue 프로젝트')
+  }
+}
+</script>
