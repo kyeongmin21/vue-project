5f6c35f73efa883db99fd1b630cd4a15a0838cef
kim kyeong min <kyeongmin21@naver.com>
2022-04-19 22:34:19 +0900

    import 시키기
    - 공통 스크립트로 생성하여 반응형으로 처리할 수 있음 (활용도 업)

--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -12,20 +12,12 @@
 </template>
 
 <script>
-import { reactive, computed, toRefs } from 'vue'
+import { plusCalculator } from '@/common.js'
 
-function plus () {
-  const state = reactive({
-    num1: 0,
-    num2: 0,
-    result: computed(() => Number(state.num1) + Number(state.num2))
-  })
-  return toRefs(state)
-}
 export default {
   name: 'CompositionAPI',
   setup () {
-    const { num1, num2, result } = plus()
+    const { num1, num2, result } = plusCalculator()
     return {
       num1, num2, result
     }
