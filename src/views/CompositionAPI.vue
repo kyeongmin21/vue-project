1ce8bdfc9d9e9bea1a5a4791a253433e91867629
kim kyeong min <kyeongmin21@naver.com>
2022-04-19 22:28:52 +0900

    함수 분리
    - v-model에서 state 없애기 / toRefs 추가

--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -2,32 +2,33 @@
   <div>
     <h2>Calculate</h2>
     <div>
-      <input type="text" v-model="state.num1" >
+      <input type="text" v-model="num1" >
       <span> + </span>
-      <input type="text" v-model="state.num2">
+      <input type="text" v-model="num2">
       <span> = </span>
-      <span>{{ state.result }}</span>
+      <span>{{ result }}</span>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, computed } from 'vue'
+import { reactive, computed, toRefs } from 'vue'
 
+function plus () {
+  const state = reactive({
+    num1: 0,
+    num2: 0,
+    result: computed(() => Number(state.num1) + Number(state.num2))
+  })
+  return toRefs(state)
+}
 export default {
   name: 'CompositionAPI',
   setup () {
-    const state = reactive({
-      num1: 0,
-      num2: 0,
-      result: computed(() => Number(state.num1) + Number(state.num2))
-    })
+    const { num1, num2, result } = plus()
     return {
-      state
+      num1, num2, result
     }
   }
 }
 </script>
-
-<style>
-</style>
