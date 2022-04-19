1af0a2af0779c9052d2d37e233b3efb3023df494
kim kyeong min <kyeongmin21@naver.com>
2022-04-19 21:19:36 +0900

    compositionAPI
    -vue3버전에 추가된 함수 기반의 API

---
+++ src/views/CompositionAPI.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Calculate</h2>
+    <div>
+      <input type="text" v-model="state.num1" @keyup="plusNumbers">
+      <span> + </span>
+      <input type="text" v-model="state.num2" @keyup="minusNumbers">
+      <span> = </span>
+      <span>{{ state.result }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue'
+
+export default {
+  name: 'calculate',
+  setup () {
+    let state = reactive({
+
+    });
+  }
+}
+</script>
+
+<style>
+</style>
