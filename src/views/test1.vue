4b205df162063c2ea73012d96024f6f32b2444f9
kim kyeong min <kyeongmin21@naver.com>
2022-04-24 20:54:54 +0900

    복습하기

--- src/views/test1.vue
+++ src/views/test1.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
-
+    <h2>calculator</h2>
+    <input type="text" v-model="state.num1">
+    <span>+</span>
+    <input type="text" v-model="state.num2">
+    <span>=</span>
+    <span>{{ state.result }}</span>
   </div>
 </template>
 
 <script>
+import { reactive, computed, toRefs } from 'vue'
+function plusCalculator () {
+  const state = reactive({
+    num1: '',
+    num2: '',
+    result: computed(() => Number(state.num1) + Number(state.num2))
+  })
+  return toRefs(state)
+}
 export default {
-  name: 'Test1',
-  computed: {
-
-  },
-  methods: {
-
+  name: 'test1',
+  setup () {
+    const { num1, num2, result } = plusCalculator()
+    return { num1, num2, result }
   }
 }
 </script>
