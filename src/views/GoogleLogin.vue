61f46ce7c07710611db696f8837660cfa4b66905
kim kyeong min <kyeongmin21@naver.com>
2022-04-27 21:05:39 +0900

    google map

--- src/views/GoogleLogin.vue
+++ src/views/GoogleLogin.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <h1>구글 로그인</h1>
-    <div id="google-signin-btn"></div>
-    <br><br>
-    <h1>구글맵</h1>
     <GoogleMap api-key="AIzaSyCcESs9ObeJsAfgHcttqttqdaOQGRQ311g"
                style="width: 100%; height: 500px"
                :center="center"
@@ -25,22 +21,7 @@ export default {
       center: { lat: 37.504449, lng: 127.048860 }
     }
   },
-  mounted () {
-    window.gapi.signin2.render('google-signin-btn', { onsuccess: this.onSignIn })
-  },
   methods: {
-    onSignIn () {
-      // eslint-disable-next-line no-undef
-      const profile = googleUser.getBasicProfile()
-      console.log(profile.getId())
-      console.log(profile.getGivenName())
-      console.log(profile.getFamilyName())
-      console.log(profile.getImageUrl())
-      console.log(profile.getEmail())
-      // eslint-disable-next-line camelcase,no-undef
-      const id_token = googleUser.getAuthResponse().id_token
-      console.log(id_token)
-    }
   }
 }
 </script>
