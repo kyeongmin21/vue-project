756b3495045a3b0d07bb98df583ba4f7f9f5a078
kim kyeong min <kyeongmin21@naver.com>
2022-04-27 19:39:12 +0900

    layout 폴더 및 파일 생성
    - header부분 공통으로 빼기 위함

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="baseWrap" >
+    <Header/>
+    <div class="wrap">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    Header
+  }
+}
+</script>
