4b581e64847a78b9f4e249e6c3cc26020c5c8ce9
kim kyeong min <kyeongmin21@naver.com>
2022-05-15 22:28:12 +0900

    params으로 데이터 받기

--- src/views/Params.vue
+++ src/views/Params.vue
@@ -1,10 +1,34 @@
 <template>
+ <div>
+   <h1>Params</h1>
 
+   <h2>params로 받은 데이터</h2>
+   <div>name: {{ $route.params.name }}</div>
+   <div>age: {{ $route.params.age }}</div>
+
+   <h2>props로 받은 데이터</h2>
+   <div>name: {{ name }}</div>
+   <div>age: {{ age }}</div>
+ </div>
 </template>
 
 <script>
 export default {
-  name: 'Params'
+  name: 'Params',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    age: {
+      type: Number,
+      default: 0
+    }
+  },
+  created () {
+    console.log(this.$route.params.name)
+    console.log(this.$route.params.age)
+  }
 }
 </script>
 
